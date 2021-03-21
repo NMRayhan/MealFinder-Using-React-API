f3da94df1336000d12e881d57451c803e7beed0d
NMRayhan <nurmdrayhan97@gmail.com>
2021-03-21 12:11:32 +0600

    Some unused Variable Used

--- src/Component/ItemShowing/ItemShowing.js
+++ src/Component/ItemShowing/ItemShowing.js
@@ -5,15 +5,19 @@ import Button from 'react-bootstrap/Button';
 
 const ItemShowing = (props) => {
     console.log(props.Meals);
-    const { idMeal, strMeal, strMealThumb, strSource, strInstructions, strTags } = props.Meals
+    const { strMeal, strMealThumb, strSource, strInstructions, strTags } = props.Meals
     console.log(strTags)
     return (
         <div className="meal-container">
             <div className="meal">
                 <h2>{strMeal}</h2>
                 <Button className="main-btn mb-3 prepare-btn" onClick={async () => {
-                    const result = await Confirm(`${strInstructions}`, `${strMeal}`);
-                }}>Prepare This Meal</Button>
+                    const result = await Confirm(`${strInstructions}`, <h1>{strMeal}</h1>);
+                    if(result){
+                        //Nothing TO Show
+                    }
+                }}
+                >Prepare This Meal</Button>
                 <img className="meal-img" src={strMealThumb} alt="" />
             </div>
             <Button className="main-btn source-btn" target="_blank" href={strSource}>Source</Button>
